ab10ca7d74677f5e9af3c1ff46003ecfecdcb246
Mahendra <mahendrapurohit9889@gmail.com>
2024-10-23 14:38:49 +0530

    React

--- ReactJs/36.ReduxSaga/src/saga.js
+++ ReactJs/36.ReduxSaga/src/saga.js
@@ -1,8 +1,8 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 import { fetchDataSuccess, fetchDataFailure } from './Reducer/Action';
-
-// Worker saga: fetch data from API
+  
+// Worker saga: fetch data from API  
 function* fetchDataSaga() {
   try {
     const response = yield call(axios.get, 'http://localhost:3000/products');
@@ -11,7 +11,7 @@ function* fetchDataSaga() {
     yield put(fetchDataFailure(error.message));
   }
 }
-
+    
 // Watcher saga: watch for the FETCH_DATA_REQUEST action
 function* watchFetchDataSaga() {
   yield takeEvery('FETCH_DATA_REQUEST', fetchDataSaga);
